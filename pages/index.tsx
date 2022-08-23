4f64fb2e602a7a776444158e250f64d4285b42e7
Michiel Leunens <michiel.leunens@gmail.com>
2022-08-23 16:45:16 +0200

    Add masonry layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextSeo } from 'next-seo';
+import Masonry from 'react-masonry-css';
 import styles from './styles/index.module.scss';
 import { Layout, Logo } from '../components/common';
 
@@ -31,6 +32,21 @@ const Home = () => {
           <span className={styles.landing__text}>un website de</span>
           <Logo />
         </div>
+      </section>
+      <section className="container py-container">
+        <Masonry
+          breakpointCols={3}
+          className="masonry-grid"
+          columnClassName="masonry-grid_column"
+        >
+          <div style={{ width: '100%', height: 500, background: 'red' }} />
+          <div style={{ width: '100%', height: 500, background: 'green' }} />
+          <div style={{ width: '100%', height: 100, background: 'blue' }} />
+          <div style={{ width: '100%', height: 300, background: 'blue' }} />
+          <div style={{ width: '100%', height: 200, background: 'orange' }} />
+          <div style={{ width: '100%', height: 400, background: 'yellow' }} />
+          <div style={{ width: '100%', height: 200, background: 'orange' }} />
+        </Masonry>
       </section>
     </>
   );
